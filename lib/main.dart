ef9f6cac86aba131807f6188145d392a0408584b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-01-29 09:10:50 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class AppState {
+  var count = 19;
+  var name = 'Mark';
+}
+
+class InheritedState extends InheritedWidget {
+  final state = AppState();
+
+  InheritedState({
+    required Widget child,
+    Key? key,
+  }) : super(key: key, child: child);
+
+  static InheritedState? of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<InheritedState>();
+
+  @override
+  bool updateShouldNotify(InheritedState oldWidget) {
+    return oldWidget.state != state;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InheritedState(child: HomePage()),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final state = InheritedState.of(context)!.state;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inherited Widget Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('count = ${state.count}'),
+            Text('name = ${state.name}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
